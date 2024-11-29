7710da3177b19354da65b7f60d0f98b710907fd3
Ferenc Pintér <pinterfer@gmail.com>
2024-11-29 13:26:42 +0100

    Remove checks already present in ctor, simplify chroma copy, copy A if exists

--- amDCT/amDCT.cpp
+++ amDCT/amDCT.cpp
@@ -6,20 +6,13 @@
 
 #define ALIGN 16
 
-
 /****************************
  * The following is the header definitions.
  * For larger projects, move this into a .h file
  * that can be included.
  ****************************/
 
-
-
 class amDCT : public GenericVideoFilter {
-  // amDCT defines the name of your filter class.
-  // This name is only used internally, and does not affect the name of your filter or similar.
-  // This filter extends GenericVideoFilter, which incorporates basic functionality.
-  // All functions present in the filter must also be present here.
 
   // define the parameter variable
   PClip pf1;
@@ -44,40 +37,17 @@ class amDCT : public GenericVideoFilter {
   int   ncpu;
 
 public:
-  // This defines that these functions are present in your class.
-  // These functions must be that same as those actually implemented.
-  // Since the functions are "public" they are accessible to other classes.
-  // Otherwise they can only be called from functions within the class itself.
-  //  env->AddFunction("amDCT", "c[pf1]c[bf1]c[quant]i[adapt]i[shift]i[matrix]i[qtype]i[sharpWPos]i[sharpWAmt]i[expand]i[sharpTPos]i[sharpTAmt]i[quality]i[brightStart]i[brightAmt]i[darkStart]i[darkAmt]i[showMask]i[T2]i[ncpu]i", Create_amDCT, 0);
 
   amDCT(PClip _child, const PClip _pf1, const PClip _bf1, const int _quant, const int _adapt, const int _shift, const int _matrix, const int _qtype, const int _sharpWPos, const int _sharpWAmt, const int _expand, const int _sharpTPos, const int _sharpTAmt, const int _quality, const int _brightStart, const int _brightAmt, const int _darkStart, const int _darkAmt, const int _showMask, const int _T2, const int _ncpu, IScriptEnvironment* env);
-  // This is the constructor. It does not return any value, and is always used,
-  //  when an instance of the class is created.
-  // Since there is no code in this, this is the definition.
-
   ~amDCT();
-  // The is the destructor definition. This is called when the filter is destroyed.
-
-
   PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
-  // This is the function that AviSynth calls to get a given frame.
-  // So when this functions gets called, the filter is supposed to return frame n.
 };
 
-/***************************
- * The following is the implementation
- * of the defined functions.
- ***************************/
 
- //Here is the actual constructor code used
+//Here is the actual constructor code used
 amDCT::amDCT(PClip _child, PClip _pf1, PClip _bf1, const int _quant, const int _adapt, const int _shift, const int _matrix, const int _qtype, const int _sharpWPos, const int _sharpWAmt, const int _expand, const int _sharpTPos, const int _sharpTAmt, const int _quality, const int _brightStart, const int _brightAmt, const int _darkStart, const int _darkAmt, const int _showMask, const int _T2, const int _ncpu, IScriptEnvironment* env) :
   GenericVideoFilter(_child), pf1(_pf1), bf1(_bf1), quant(_quant), adapt(_adapt), shift(_shift), matrix(_matrix), qtype(_qtype), sharpWPos(_sharpWPos), sharpWAmt(_sharpWAmt), expand(_expand), sharpTPos(_sharpTPos), sharpTAmt(_sharpTAmt), quality(_quality), brightStart(_brightStart), brightAmt(_brightAmt), darkStart(_darkStart), darkAmt(_darkAmt), showMask(_showMask), T2(_T2), ncpu(_ncpu) {
 
-  // This is the implementation of the constructor.
-  // The child clip (source clip) is inherited by the GenericVideoFilter,
-  //  where the following variables gets defined:
-  //   PClip child;   // Contains the source clip.
-  //   VideoInfo vi;  // Contains video-info on the source clip.
   if (!vi.IsYV12()) // is input planar?
     env->ThrowError("amDCT: input to filter must be in YV12");
 
@@ -141,6 +111,7 @@ amDCT::amDCT(PClip _child, PClip _pf1, PClip _bf1, const int _quant, const int _
 
 }
 
+
 // This is where any actual destructor code used goes
 amDCT::~amDCT() {
   // This is where you can deallocate any memory you might have used.
@@ -148,58 +119,32 @@ amDCT::~amDCT() {
 
 
 PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
-  // This is the implementation of the GetFrame function.
-  // See the header definition for further info.
 
   PVideoFrame src = child->GetFrame(n, env);
+
+  PVideoFrame dst = env->NewVideoFrame(vi);
+
+  const unsigned char* srcp = src->GetReadPtr();
 
   //  PVideoFrame pf1Src = NULL;  // WAS src
   //  if (pf1 != NULL) pf1Src = pf1->GetFrame(n, env);
   //  PVideoFrame bf1Src = NULL;
   //  if (bf1 != NULL) bf1Src = bf1->GetFrame(n, env);
 
-
-     // Request frame 'n' from the child (source) clip.
-  PVideoFrame dst = env->NewVideoFrame(vi);
-  // Construct a frame based on the information of the current frame
-  // contained in the "vi" struct.
-
-  const unsigned char* srcp = src->GetReadPtr();
-
   //  const unsigned char* pf1Srcp = NULL; // WAS srcp
   //  const unsigned char* bf1Srcp = NULL;
   //  if (pf1 != NULL)  pf1Srcp = pf1Src->GetReadPtr();
   //  if (bf1 != NULL)  bf1Srcp = bf1Src->GetReadPtr();
 
-    //const unsigned char* QmaskSrcp  = srcp; //QmaskSrc->GetReadPtr();
-    // Request a Read pointer from the source frame.
-    // This will return the position of the upper-left pixel in YUY2 images,
-    // and return the lower-left pixel in RGB.
-    // RGB images are stored upside-down in memory.
-    // You should still process images from line 0 to height.
+  //const unsigned char* QmaskSrcp  = srcp; //QmaskSrc->GetReadPtr();
 
   unsigned char* dstp = dst->GetWritePtr();
-  // Request a Write pointer from the newly created destination image.
-  // You can request a writepointer to images that have just been
-  // created by NewVideoFrame. If you receive a frame from PClip->GetFrame(...)
-  // you must call env->MakeWritable(&frame) to receive a valid write pointer.
-
   const int dst_pitch = dst->GetPitch();
-  // Requests pitch (length of a line) of the destination image.
-  // For more information on pitch see: http://www.avisynth.org/index.php?page=WorkingWithImages
-  // (short version - pitch is always equal to or greater than width to allow for seriously fast assembly code)
-
   const int dst_width = dst->GetRowSize();
-  // Requests row-size (number of used bytes in a line.
-  // See the link above for more information.
-
   const int dst_height = dst->GetHeight();
-  // Requests the height of the destination image.
-
   const    int src_pitch = src->GetPitch();
   unsigned int src_width = src->GetRowSize();
   unsigned int src_height = src->GetHeight();
-
 
   // We want pf1 to be the size of the src frame that was passed in.
 //         int pf1_pitch  = 0; // was src_pitch;
@@ -223,72 +168,7 @@ PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
   // We check the src frame to make sure it is mod16 width.
   // Mod8 is required for the core amDCT algorithm to work.
   // Mod16 keeps the start of each row aligned at multiples of 16 bytes to optimize mmx
-  unsigned int w, h;
-  //w = src_width;
-  //if (src_width != (w>>4)<<4) src_width = ((src_width + 15)>>4)<<4;
   src_width = (src_width + 15) & ~15;  // 15 = ALIGN-1  // ALIGN must be a power of 2
-
-  // check parameters for valid values
-  if ((quant < 0) || (quant > 31))
-    env->ThrowError("amDCT: quant out of range");
-
-  if ((adapt < 0) || (adapt > 31))
-    env->ThrowError("amDCT: adapt out of range");
-
-  if ((shift < 0) || (shift > 5))
-    env->ThrowError("amDCT: shift out of range");
-
-  if ((matrix < 0) || (matrix > 255))
-    env->ThrowError("amDCT: matrix out of range");
-
-  if ((qtype < 1) || (qtype > 24))
-    //  if ((qtype<1) || (qtype>14))
-    env->ThrowError("amDCT: qtype out of range (1-4, 11-14)");
-
-  if (((sharpWPos < 0) || (sharpWPos > 7)) && (sharpWPos != 255))
-    env->ThrowError("amDCT: sharpWPos out of range (0-7)");
-
-  if (((sharpWAmt < 0) || (sharpWAmt > 31)) && (sharpWAmt != 255))
-    env->ThrowError("amDCT: sharpWAmt out of range (0-31)");
-
-  if ((expand < 0) || (expand > 31))
-    env->ThrowError("amDCT: expand out of range (0 to 31)");
-
-  if (((sharpTPos < 0) || (sharpTPos > 7)) && (sharpTPos != 255))
-    env->ThrowError("amDCT: sharpTPos out of range (0-7)");
-
-  if (((sharpTAmt < 0) || (sharpTAmt > 31)) && (sharpTAmt != 255))
-    env->ThrowError("amDCT: sharpTAmt out of range (0-31)");
-
-  if ((quality < 1) || (quality > 6))
-    env->ThrowError("amDCT: quality out of range (1 to 6 default=2)");
-
-  if ((sharpTPos != 0 && sharpTPos != 255) && sharpWPos != 0 && (sharpWPos != 255) && (sharpWPos > sharpTPos))
-    env->ThrowError("amDCT: sharpTPos must be 0 or >= sharpWPos");
-
-  if ((brightStart < 0) || (brightStart > 255))
-    env->ThrowError("amDCT: brightStart out of range (0-255)");
-
-  if ((brightAmt < 0) || (brightAmt > 31))
-    env->ThrowError("amDCT: brightAmt out of range (0-31)");
-
-  if ((darkStart < 0) || (darkStart > 255))
-    env->ThrowError("amDCT: darkStart out of range (0-255)");
-
-  if ((darkAmt < 0) || (darkAmt > 31))
-    env->ThrowError("amDCT: darkAmt out of range (0-31)");
-
-  if ((showMask < 0) || (showMask > 255))
-    env->ThrowError("amDCT: showMask out of range (0-255)");
-
-  if ((T2 < 0) || (T2 > 255))
-    env->ThrowError("amDCT: T2 out of range (0-255)");
-
-  if ((ncpu < 1) || (ncpu > 4))
-    env->ThrowError("amDCT: ncpu out of range (1-4)");
-
-
-
 
   // Set the "dependency default values".
   // Dependency default values depend on the values of other arguments.
@@ -302,8 +182,6 @@ PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
   // If neither sharpWPos or sharpTPos have been set then use their defaults.
   if (sharpWPos == 255 && sharpTPos == 255 && sharpWAmt != 255)  sharpWPos = 5;
   if (sharpWPos == 255 && sharpTPos == 255 && sharpTAmt != 255)  sharpTPos = 7;
-
-
 
   if (sharpWAmt == 255 && sharpTAmt != 255) sharpWAmt = sharpTAmt;
   if (sharpTAmt == 255 && sharpWAmt != 255) sharpTAmt = sharpWAmt;
@@ -328,11 +206,8 @@ PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
     sharpWAmt = sharpTAmt;
   }
 
-
   if (brightStart >= 255) brightAmt = 0;
   if (darkStart <= 0)   darkAmt = 0;
-
-
 
   //  if (pf1 != NULL && ((pf1_width != src_width) || (pf1_height != src_height)))// {
   //    env->ThrowError("pf1 size != Src size");
@@ -343,13 +218,6 @@ PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
   //    env->ThrowError("bf1 size != Src size");
   //    return src;
   //    }
-
-
-  //  PClip AsClip() const { _ASSERTE(IsClip()); return IsClip()?clip:0; }
-  //{ _ASSERTE(1); return (dst) ? dst : 0; }
-  //const { _ASSERTE(1)}; return 1 ? 1 : 0;
-
-
 
   if (vi.IsYV12()) {
 
@@ -372,13 +240,8 @@ PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
     srcp = src->GetReadPtr();
     dstp = dst->GetWritePtr();
 
-
     unsigned int   height = src_height;
     unsigned int   width = src_width;
-
-
-
-
     /*
     //      FUTURE TRY MULTIPLE MOTION COMPENSATED FRAMES from MVTools2
         unsigned char       *pf1Buf  = NULL;
@@ -403,8 +266,6 @@ PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
         }
     */
 
-
-
     unsigned char* psrc = (unsigned char*)srcp;
     height = src_height;
     width = src_width;
@@ -414,53 +275,23 @@ PVideoFrame __stdcall amDCT::GetFrame(int n, IScriptEnvironment* env) {
         //if (aaDCT(psrc, ppf1, pbf1, dstp, height, width, dst_height, dst_pitch, quant, adapt, shift, matrix, qtype, expand, sharpWPos, sharpWAmt, sharpTPos, sharpTAmt, quality, brightStart, brightAmt, showMask, T2, ncpu) > 0) {
     if (amDCTmain(psrc, dstp, height, width, src_pitch, dst_height, dst_width, dst_pitch, quant, adapt, shift, matrix, qtype, expand, sharpWPos, sharpWAmt, sharpTPos, sharpTAmt, quality, brightStart, brightAmt, darkStart, darkAmt, showMask, T2, ncpu) == 1) {
       env->ThrowError("amDCT: out of memory");
-
     }
 
     // end of Y plane Code
 
-
-
-    // This section of code deals with the U and V planes of planar formats (e.g. YV12)
-    // So first of all we have to get the additional info on the U and V planes
-
+    // Copy U and V planes
     const unsigned int dst_pitchUV = dst->GetPitch(PLANAR_U);   // The pitch,height and width information
-    const unsigned int dst_widthUV = dst->GetRowSize(PLANAR_U); // is guaranteed to be the same for both
-    const unsigned int dst_heightUV = dst->GetHeight(PLANAR_U);   // the U and V planes so we only the U
     const unsigned int src_pitchUV = src->GetPitch(PLANAR_U);   // plane values and use them for V as
     const unsigned int src_widthUV = src->GetRowSize(PLANAR_U); // well
     const unsigned int src_heightUV = src->GetHeight(PLANAR_U);   //
-
-    //Copy U plane src to dst
-    srcp = src->GetReadPtr(PLANAR_U);
-    dstp = dst->GetWritePtr(PLANAR_U);
-
-    for (h = 0; h < src_heightUV; h++) {
-      for (w = 0; w < src_widthUV; w++)
-        //        *(dstp + w) = 128;   // Debug This returns greyscale
-        *(dstp + w) = *(srcp + w);
-      srcp = srcp + src_pitchUV;
-      dstp = dstp + dst_pitchUV;
+    // no problem if Y only
+    env->BitBlt(dst->GetWritePtr(PLANAR_U), dst_pitchUV, src->GetReadPtr(PLANAR_U), src_pitchUV, src_widthUV, src_heightUV);
+    env->BitBlt(dst->GetWritePtr(PLANAR_V), dst_pitchUV, src->GetReadPtr(PLANAR_V), src_pitchUV, src_widthUV, src_heightUV);
+    // and Alpha if Any
+    if (vi.IsPlanarRGBA() || vi.IsYUVA()) {
+      env->BitBlt(dst->GetWritePtr(PLANAR_A), dst->GetPitch(PLANAR_A), src->GetReadPtr(PLANAR_A),
+        src->GetPitch(PLANAR_A), src->GetRowSize(PLANAR_A), src->GetHeight(PLANAR_A));
     }
-    // end copy U plane src to dst
-
-
-
-    //Copy V plane src to dst
-    srcp = src->GetReadPtr(PLANAR_V);
-    dstp = dst->GetWritePtr(PLANAR_V);
-
-    for (h = 0; h < src_heightUV; h++) {
-      for (w = 0; w < src_widthUV; w++)
-        //        *(dstp + w) = 128;   // debug This returns greyscale
-        *(dstp + w) = *(srcp + w);
-      srcp = srcp + src_pitchUV;
-      dstp = dstp + dst_pitchUV;
-    }
-    // end copy V plane src to dst
-
-
-
 
 //  if (pf1 != NULL) _aligned_free(pf1Buf);
 //  if (bf1 != NULL) _aligned_free(bf1Buf);
